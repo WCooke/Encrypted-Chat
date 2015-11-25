5ab1e7684242e7dd5f4de66145688f4c6919cf47
WCooke <ow-nag3@live.co.uk>
2015-11-25 14:51:29 +0000

    Update GUI Python Client.py
    Added Alias Input, also Mark added the ASCII Art function :)

--- GUI Python Client.py
+++ GUI Python Client.py
@@ -7,7 +7,7 @@ import threading
 
 
 def Main_GUI():
-    global entr, text1, root, ip_entr, port_entr
+    global entr, text1, root, ip_entr, port_entr, Alias_entr
     root=Tk()
     root.title('Chat Client')
     Button(root, text='Send', command=send).pack(side= BOTTOM)
@@ -21,22 +21,25 @@ def Main_GUI():
     text1.pack(side= LEFT, fill =BOTH)
     scr.config(command=text1.yview)
     scr.pack(side=LEFT, fill=Y)
-    
-    
+
     seperator= Frame(root)
     seperator.pack(side=LEFT)
+    Alias =Label(seperator, text='Alias')
+    Alias.grid(row=0, column=0)
+    Alias_entr =Entry(seperator)
+    Alias_entr.grid(row=0, column=1)
     ip =Label(seperator, text='IP')
-    ip.grid(row=0, column=0)
+    ip.grid(row=1, column=0)
     ip_entr =Entry(seperator)
-    ip_entr.grid(row=0, column=1)
+    ip_entr.grid(row=1, column=1)
     ip_entr.insert(END, '192.168.17.129')
     ip_entr.focus()
     port =Label(seperator, text='Port')
-    port.grid(row=1, column=0)
+    port.grid(row=2, column=0)
     port_entr =Entry(seperator)
-    port_entr.grid(row=1, column=1)
+    port_entr.grid(row=2, column=1)
     port_entr.insert(END, '8421')
-    Button(seperator, text='Connect', command=main).grid(row=2, column=1)
+    Button(seperator, text='Connect', command=main).grid(row=3, column=1)
     port_entr.focus()
     root.mainloop()
     
@@ -51,6 +54,7 @@ def send():
     except:
         print_("Problem while sending text...")
 
+
 def print_(data):
     global line11
     if len(data) > 0:
@@ -61,26 +65,66 @@ def print_(data):
     text1.mark_set(INSERT, line11)
     text1.config(state=DISABLED)
 
+
+
+def AsciiArt (inputstring):
+    """takes user input and converts allows user to paste ascii emojies quickly using special
+    comands"""
+    import os
+    files = [f for f in os.listdir("AsciiArt/") if f.endswith('.txt')]
+    names2 = list()
+    for file in files:
+        h = "!"+file
+        names2.append (h.replace(".txt", ""))
+    if inputstring == "!help":
+        names = list()
+        for file in files:
+            names.append (file.replace("AsciiArt/", "").replace(".txt", ""))
+        print_("Valid Picture Commands are:")
+        for file in names:
+            print_(file)
+    elif inputstring in names2:
+        file = inputstring.replace("!", "")
+        filepath = "AsciiArt/" + file + ".txt"
+        output = open(filepath, 'r').read()
+        server_socket.send(output)
+    else:
+        print_("This is not a valid command! Try '!help'")
+
+
 def main():
     Server_Running_thread =threading.Thread(target=Server)
+    Server_Running_thread.daemon =True
     Server_Running_thread.start()
 
+
+def create_alias(server_socket):
+    requested_alias = ""
+    while True:
+        if not requested_alias:
+            requested_alias = Alias_entr.get()
+            server_socket.send(requested_alias)
+        else:
+            server_decision = server_socket.recv(4096)
+            if server_decision == "Accepted":
+                return print_("Alias Accepted")
+            elif server_decision == "Rejected":
+                return print_("Alias Rejected")
+                requested_alias = ""
+
     
-
-
-
 def Server():
     global server_socket
     host = ip_entr.get()
     port = int(port_entr.get())
-     
+
     server_socket = socket.socket()
     server_socket.settimeout(2)
     server_socket.connect((host, port))
-    #here
-    print_("Connected to remote host. You can now send messages")
-    
-     
+    print_("Connected to remote host.")
+    create_alias(server_socket)
+    print_("You can now send messages.")
+
     while True:
         socket_list = [sys.stdin, server_socket]
         read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
@@ -94,10 +138,10 @@ def Server():
                     sys.exit()
                 else:
                     print_(data)
-                    
+
             else:
                 server_socket.send(stext)
-     
+
 line11=1
 if __name__ == "__main__":
     Main_GUI()
